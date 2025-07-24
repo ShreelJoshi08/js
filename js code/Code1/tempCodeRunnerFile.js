3c1b30c9ed9a615d666a520a303bdd071f68c6ee
Shreel23 <shreelj59@.com>
2025-07-24 13:10:16 +0530

    ecom store

---
+++ js code/Code1/tempCodeRunnerFile.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
+
+
+/////////////////////////////////
+// SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+console.log(dataObj)
+// const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+// console.log(slugs);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+  console.log(pathname);
+  //const pathname = req.url;
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+    //res.end('Welcome To Overview Page');
+    
+    // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    const product = dataObj[query.id];
+    let output = replaceTemplate(tempProduct, product);
+    res.end(output);
+    //res.end('Welcome To Product Page');
+    
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
+    //res.end('Welcome To API Page');
+    
+    // Not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+server.listen(9000, '127.0.0.1', () => {
+  console.log('172.0.0.1:9000 is running');
+});
